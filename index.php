2f998651d3e0324fbd0b3426fe59a2e0d81da117
LoganLS <logan_56@hotmail.fr>
2018-10-24 11:40:10 +0200

    Creation arbo dossier

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    $title='';
+    $description='';
+?>
+<?php include_once('layouts/header.php');?>
+<main>
+            
+</main>
+<?php include_once('layouts/footer.php');?>
---
+++ layouts/head.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<title><?php echo $title;?></title>
+<meta description="<?php echo $description;?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="scss/style.css">
+<!-- JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
